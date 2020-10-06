eebf1af53119693bf6de9bab4ba401c79f078941
Mary Baker <meb601@gmail.com>
2020-10-06 12:26:56 -0400

    Big bang!

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "and welcome" });
+});
+
+server.listen(PORT, () => console.log(`API running on ${PORT}`));
